16a550a94c8a1574c9e5eb57151548512495dbd4
AlphaCyber <amir1300sobatdar@gmail.com>
2017-03-17 11:44:21 +0330

    AlphaTab

--- files/base.lua
+++ files/base.lua
@@ -19,11 +19,11 @@ reload()
 function tdcli_update_callback(data)
   tabchi.update(data, tabchi_id)
   if data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/reload" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
-    reload()
-    bot.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
+     reload()
+     bot.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
   elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
-    io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
-    reload()
-    bot.sendMessage(data.message_.chat_id_, 0, 1, "*Updates Received And Bot Reloaded*", 1, "md")
+     io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
+     reload()
+     bot.sendMessage(data.message_.chat_id_, 0, 1, "*Updates Received And Bot Reloaded*", 1, "md")
   end
 end
